372b94240a3a0975b588975d60316b1ce58b6d9d
David Daza <03dazal@gmail.com>
2018-02-15 18:58:04 -0500

    Tips page

--- src/js/herramientas.js
+++ src/js/herramientas.js
@@ -1,18 +1,20 @@
 const key = 'https://docs.google.com/spreadsheets/d/1i9CAbsQDsp3OxiWZ-1iBY21DahR4PA4a7gX7bK4N8yA/edit?usp=sharing';
 
-Tabletop.init({
-  key: key,
-  callback: function (data, tabletop) {
-    const tools = tabletop.sheets('Sheet1')
-      .all()
-      .map(row => ({ name: row.name, description: row.description_es, org: row.org, pricing: row.pricing, link: row.link }))
-    ;
-
-    const vm = new Vue({
-      el: '#renderer',
-      data: {
-        tools: tools
-      }
-    })
-  }
+window.addEventListener('load', function () {
+  Tabletop.init({
+    key: key,
+    callback: function (data, tabletop) {
+      const tools = tabletop.sheets('Sheet1')
+        .all()
+        .map(row => ({ name: row.name, description: row.description_es, org: row.org, pricing: row.pricing, link: row.link }))
+      ;
+  
+      const vm = new Vue({
+        el: '#renderer',
+        data: {
+          tools: tools
+        }
+      })
+    }
+  })
 })
